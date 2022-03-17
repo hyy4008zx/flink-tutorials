3de31a690dc85d7bd30aeb0c327c4fb6d1fbc177
Admin <3suo@chci.cn>
2022-03-17 11:07:42 +0800

    test

---
+++ streaming/src/main/java/com/flink/streaming/operators/AAA.java
@@ -0,0 +1,12 @@
+package com.flink.streaming.operators;
+
+/**
+ * @Author: huyanyang
+ * @Date: 2022/03/17/11:03
+ * @Description:
+ */
+public class AAA {
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
+}
